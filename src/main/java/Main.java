975122e8580226fd7294278fbc5d05c9a88951db
Ovsyannikov_M <maksim.ov@mail.ru>
2024-04-29 23:28:39 +0300

    create commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        int amountOfGuests = -1;
+        ArrayList<Product> productsList = new ArrayList<>();
+
+        while(amountOfGuests <= 1){
+            System.out.println("Введите число гостей:");
+            if(scanner.hasNextInt()) {
+                amountOfGuests = scanner.nextInt();
+            } else {
+                System.out.println("Необходимо ввести целое число. Повторите попытку.");
+                scanner.next();
+                continue;
+            }
+            if(amountOfGuests == 1){
+                System.out.println("Нет смысла сичтать, если Вы один. Повторите попытку.");
+            } else if(amountOfGuests < 1){
+                System.out.println("Некорректное число. Повторите попытку.");
+            }
+        }
+
+        while(true){
+            System.out.println("Введите товар или 'Завершить', для завершения:");
+            String name = scanner.next();
+            if(name.equalsIgnoreCase("Завершить")){
+                break;
+            }
+            System.out.println("Введите цену товара (рубли,копейки):");
+            if(scanner.hasNextDouble()) {
+                double cost = scanner.nextDouble();
+                if(cost < 0){
+                    System.out.println("Необходимо ввести положительное дробное число. Повторите попытку.");
+                    continue;
+                }
+                productsList.add(new Product(name, cost));
+            } else {
+                System.out.println("Необходимо ввести дробное число через запятую. Повторите попытку.");
+                scanner.next();
+            }
+        }
+
+        System.out.println("Добавленные товары:");
+        Utils.printList(productsList);
+
+        double sum = Utils.findSum(productsList);
+
+        System.out.printf((Utils.formatSum) + "%n", sum);
+        System.out.printf((Utils.formatEveryPay) + "%n", sum/amountOfGuests);
     }
 }
---
+++ src/main/java/Utils.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Utils {
+
+    static String formatSum = "Сумма: %.2f";
+    static String formatEveryPay  = "Каждый должен заплатить:  %.2f";
+
+    private static String getRub(double cost){
+        int costInt = (int) cost;
+        if(costInt>10 & costInt<20){
+            return "рублей";
+        }
+        return switch (costInt%10) {
+            default -> "рубль";
+            case 2, 3, 4 -> "рубля";
+            case 5, 6, 7, 8, 9, 0 -> "рублей";
+        };
+    }
+
+    public static void printList(ArrayList<Product> productsList) {
+        for(Product product: productsList){
+            System.out.println(product.getFormatString()+" "+getRub(product.cost));
+        }
+    }
+
+    public static double findSum(ArrayList<Product> productsList){
+        double sum = 0.0;
+        for(Product product: productsList){
+            sum+=product.cost;
+        }
+        return sum;
+    }
+}
